66eedf9fbcfb38eb01cbb402e750da34c5d852d1
MarvinL.com <contact@marvinl.com>
2021-05-06 22:02:45 -0400

    Update Lieu controller w/ create Lieu + Avis

--- modules/back/api/lieu/controllers/lieu.js
+++ modules/back/api/lieu/controllers/lieu.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const {sanitizeEntity} = require('strapi-utils')
 
 module.exports = {
   async random(ctx) {
@@ -12,5 +13,34 @@ module.exports = {
     const getRandomId = (max) => Math.floor(Math.random() * Math.floor(max))
 
     return lieux.find(lieu => lieu.id === ids[getRandomId(ids.length)])
+  },
+  async create(ctx) {
+    const {lieu} = ctx.request.body;
+    const {nom, eclaireur, region, city, localisation, avis} = lieu
+
+    const newLieu = await strapi.services.lieu.create({
+      nom,
+      eclaireur,
+      region: region.id,
+      commune: city.id,
+      localisation
+    })
+
+    const {accessible, passage, nocturne, public: isPublic, note, message, links} = avis
+
+    const newAvis = await strapi.services.avis.create({
+      accessible,
+      passage,
+      accesNocturne: nocturne,
+      note,
+      public: isPublic,
+      isEclaireur: true,
+      eclaireur,
+      lieu: newLieu.id,
+      message,
+      links
+    })
+
+    return sanitizeEntity(await strapi.services.lieu.findOne({id: newLieu.id}), {model: strapi.models.lieu})
   }
 };
